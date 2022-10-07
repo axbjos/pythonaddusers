94029f2daf9dbac64fa9ff05b536f763e298be42
Joe Axberg <joeaxberg@axnetlabs.org>
2022-10-07 09:24:03 -0500

    update create-users.py

--- create-users.py
+++ create-users.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
-print("some text")
-print("some other text")
 
-for i in range(10):
-    print(i)
+''' Add Users Program
+
+    Author: Joe Axberg
+    Create Date: 10/7/2022
+    Last Modified: 10/7/2022
+    
+    Description: reads the input file line by line
+    and adds the user to the system...blah
+'''
+
+import os
+import sys
+import re
+
+print("Processing Users...")
+
+
+# processing the file by a linux redirect
+#  ./create-users.py < create-users.input
+# each line of the file is processed in this loop
+for line in sys.stdin:
+    print(line)
